494c2c67f2d73f5906f8fd9af66f95ae70df5e26
rontu mohonto <newscenery28@gmail.com>
2025-12-30 02:45:08 +0600

    cart page working

--- lib/screen/Cart/UI.dart
+++ lib/screen/Cart/UI.dart
@@ -17,9 +17,7 @@ class _CartState extends State<Cart> {
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Color(0xFFFF4F4F),
-                Color(0xFFFF9A37)],
+              colors: [Color(0xFFFF4F4F), Color(0xFFFF9A37)],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
@@ -31,6 +29,89 @@ class _CartState extends State<Cart> {
           color: Colors.white,
           fontSize: 20,
           fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              gradient: LinearGradient(
+                colors: [Color(0xFFFF4F4F), Color(0xFFFF9A37)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 6,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              spacing: 16,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://eplay.coderangon.com/storage/{product['image']}",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 6,
+                    children: [
+                      CustomText(
+                        text: "{product['title']}",
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      CustomText(
+                        text: "Brand: {product['brand']}",
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                      Row(
+                        spacing: 10,
+                        children: [
+                          CustomText(
+                            text: "BDT {product['price']}",
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          CustomText(
+                            text: "{product['old_price']}",
+                            decorationText: TextDecoration.lineThrough,
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
