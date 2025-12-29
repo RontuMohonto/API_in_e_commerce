0daef11bfcfae131c3e714f4de930717f2c63cb6
rontu mohonto <newscenery28@gmail.com>
2025-12-29 10:45:04 +0600

    screen making

--- lib/screen/Shipping_info/UI.dart
+++ lib/screen/Shipping_info/UI.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dada_garments_full_with_api/controller/widgets/SignUpName_Widget.dart';
+import 'package:dada_garments_full_with_api/controller/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../controller/widgets/text.dart';
-
-
-
-
 
 class ShippingScreen extends StatefulWidget {
   const ShippingScreen({super.key});
@@ -14,6 +16,15 @@ class ShippingScreen extends StatefulWidget {
 }
 
 class _ShippingScreenState extends State<ShippingScreen> {
+
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController nameC = TextEditingController();
+  TextEditingController phoneC = TextEditingController();
+  TextEditingController streetC = TextEditingController();
+  TextEditingController upazillaC = TextEditingController();
+  TextEditingController districtC = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +32,7 @@ class _ShippingScreenState extends State<ShippingScreen> {
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Color(0xFFFF4F4F),
-                Color(0xFFFF9A37)],
+              colors: [Color(0xFFFF4F4F), Color(0xFFFF9A37)],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
@@ -35,6 +44,96 @@ class _ShippingScreenState extends State<ShippingScreen> {
           color: Colors.white,
           fontSize: 20,
           fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 10,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomText(
+                text: "Let's Start",
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              SizedBox(height: 5),
+              //Name
+              NameFormField(
+                nameC: nameC,
+                validator: (v) {
+                  if (v == null || v == "") {
+                    return "Please enter Name";
+                  }
+                  return null;
+                },
+                title: "name",
+              ),
+              //Phone
+              NameFormField(
+                nameC: phoneC,
+                validator: (v) {
+                  if (v == null || v == "") {
+                    return "Please enter Phone";
+                  }
+                  return null;
+                },
+                title: "Phone",
+              ),
+              //Street
+              NameFormField(
+                nameC: streetC,
+                validator: (v) {
+                  if (v == null || v == "") {
+                    return "Please enter Phone";
+                  }
+                  return null;
+                },
+                title: "Street",
+              ),
+              //"Upazila
+              NameFormField(
+                nameC: upazillaC,
+                validator: (v) {
+                  if (v == null || v == "") {
+                    return "Please enter Phone";
+                  }
+                  return null;
+                },
+                title: "Upazilla",
+              ),
+              //District
+              NameFormField(
+                nameC: districtC,
+                validator: (v) {
+                  if (v == null || v == "") {
+                    return "Please enter District";
+                  }
+                  return null;
+                },
+                title: "District",
+              ),
+              CustomButton_widget(title: "Next", onTap: ()async{
+                if(!_formkey.currentState!.validate()){
+                  return;
+                }
+                var data = {
+                  "name" : nameC.text,
+                  "phone" : phoneC.text,
+                  "street" : streetC.text,
+                  "upazilla" : upazillaC.text,
+                  "district" : districtC.text,
+                };
+                log("====${data}====");
+                FlutterSecureStorage storage = FlutterSecureStorage();
+                await storage.write(key: "shipping", value: jsonEncode(data));
+                Navigator.pop(context);
+              })
+            ],
+          ),
         ),
       ),
     );
